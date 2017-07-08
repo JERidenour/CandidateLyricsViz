ffd691b41220acc03473e55e4044c4b8e3a81c82
Master Control <fireflightning@gmail.com>
2017-07-08 09:26:32 -0400

    added some psuedocode comments on CreateAndRun

--- src/main/scala/viz/lyrics/CreateAndRun.scala
+++ src/main/scala/viz/lyrics/CreateAndRun.scala
@@ -7,12 +7,17 @@ object CreateAndRun {
     def main(args: Array[String]) {
 
         // load the data
-        val reader = CSVReader.open(new File("src/main/resources/genius_hip_hop_lyrics.csv"))
+        val reader = CSVReader
+            .open(new File("src/main/resources/genius_hip_hop_lyrics.csv"))
         val data = reader.all()
         reader.close()        
 
         // pick out unique lists
         val artists = data.map(x => x.apply(3)).distinct.tail
         val candidates = data.map(x => x.apply(1)).distinct.tail
+
+        // create a node for each artist
+        // create verticies
+        // print the dot file
     }
 }
