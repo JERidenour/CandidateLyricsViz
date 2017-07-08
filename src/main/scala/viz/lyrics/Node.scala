6b6b2063fedb2ce728e845de8c33f1847634a7b1
Master Control <fireflightning@gmail.com>
2017-07-08 08:45:00 -0400

    improved Node.toString with triple quotes

--- src/main/scala/viz/lyrics/Node.scala
+++ src/main/scala/viz/lyrics/Node.scala
@@ -4,24 +4,27 @@ class Node {
 
     // fields
     private var _name = ""
+    private var _label = ""
 
     // getters & setters
     def name = _name
+    def label = _label
 
     def name_= (in:String):Unit = _name = in
+    def label_= (in:String):Unit = _label = in
 
     override def toString: String = {
-        s"$name"
+        s"""$name [label = "$label"]"""
     }
 }
 
 object Node {
 
     // basic constuctor
-    def apply(nm: String) = {
+    def apply(lab: String) = {
 
         var n = new Node 
-        n.name = nm
+        n.label = lab 
         n
 
     }
