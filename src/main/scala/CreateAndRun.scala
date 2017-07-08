7a4a12b3e1ad579dc9b7a79847a85864df264a82
Master Control <fireflightning@gmail.com>
2017-07-07 22:03:32 -0400

    save reader list to val

--- src/main/scala/CreateAndRun.scala
+++ src/main/scala/CreateAndRun.scala
@@ -5,7 +5,8 @@ object CreateAndRun {
     def main(args: Array[String]) {
 
         val reader = CSVReader.open(new File("src/main/resources/genius_hip_hop_lyrics.csv"))
-        
+        val data = reader.all()
+        reader.close()        
 
     }
 }
