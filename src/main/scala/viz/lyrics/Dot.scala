154e85c44fbd789fa9e30dc76dd16809d114540d
Master Control <fireflightning@gmail.com>
2017-07-09 08:14:54 -0400

    added preamble to Dot

--- src/main/scala/viz/lyrics/Dot.scala
+++ src/main/scala/viz/lyrics/Dot.scala
@@ -2,12 +2,20 @@ package viz.lyrics
 
 class Dot {
 
-
+    private var _preamble = "graph g { " +
+        "graph [overlap=false outputorder=edgesfirst];" +
+        "node [style=filled fillcolor=white];"
+    
+    def preamble = _preamble
+    
+    def preamble_= (in:String):Unit = _preamble = in
 
 }
 
 object Dot {
 
+    def apply()={
 
 
+    }
 }
