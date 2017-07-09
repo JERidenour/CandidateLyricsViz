b89c4491b0f4fc284cb01285e629a64c6654e115
Master Control <fireflightning@gmail.com>
2017-07-09 16:16:26 -0400

    added colors for candidates

--- src/main/scala/viz/lyrics/CreateAndRun.scala
+++ src/main/scala/viz/lyrics/CreateAndRun.scala
@@ -15,13 +15,18 @@ object CreateAndRun {
         // pick out unique lists
         val artists = data.map(x => x.apply(3)).distinct.tail
         val candidates = data.map(x => x.apply(1)).distinct.tail
+        val candidateColors = Array("#b06660", "#d9a88f", "#ca8f42", 
+            "#ab9c73", "#537703", "#9baf8e"), "#6a7d8e", "#dbad72")
 
         val dot = new Dot()
 
         // create a node for each artist
         artists.foreach { dot.nodeBuffer += Node.apply(_)  }
         
-        // create verticies
+        // for each candidate get it's subset
+        // get the artists for that subset
+        // for each artist get the companion artist set
+        // create vertex for each such pair
         // print the dot file
     }
 }
