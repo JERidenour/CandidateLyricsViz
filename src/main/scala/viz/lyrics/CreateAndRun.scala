85fe54187a35a9fc5a237a0c9fd404cec44f4f60
Master Control <fireflightning@gmail.com>
2017-07-09 16:37:56 -0400

    added main algorithm to CreateAndRun

--- src/main/scala/viz/lyrics/CreateAndRun.scala
+++ src/main/scala/viz/lyrics/CreateAndRun.scala
@@ -16,17 +16,36 @@ object CreateAndRun {
         val artists = data.map(x => x.apply(3)).distinct.tail
         val candidates = data.map(x => x.apply(1)).distinct.tail
         val candidateColors = Array("#b06660", "#d9a88f", "#ca8f42", 
-            "#ab9c73", "#537703", "#9baf8e"), "#6a7d8e", "#dbad72")
+            "#ab9c73", "#537703", "#9baf8e", "#6a7d8e", "#dbad72")
 
         val dot = new Dot()
 
         // create a node for each artist
         artists.foreach { dot.nodeBuffer += Node.apply(_)  }
         
-        // for each candidate get it's subset
-        // get the artists for that subset
-        // for each artist get the companion artist set
-        // create vertex for each such pair
-        // print the dot file
+        for( c <- 0 to (candidates.length-1) ){
+
+            // for each candidate get it's subset
+            val subset = data.filter( (r:List[String]) => r.apply(1) == c  ) 
+        
+            // get the artists for that subset
+            val subArtists = subset.map(x => x.apply(3)).distinct.tail
+
+            for( sa <- subArtists){
+            
+                // for each artist get the companion artist set
+                val compArtists = subArtists.filter( (a:String) => a != sa  ) 
+
+                for(sac <- compArtists){
+
+                    // create vertex for each such pair
+                    dot.vertexBuffer += Vertex.apply(sa, sac, candidateColors.apply(c))
+                }
+            }
+        }
+        
+        println(dot.toString)
+
+
     }
 }
