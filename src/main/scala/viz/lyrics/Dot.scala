18019f7874f4315039a3fc37fb930be903164fb2
Master Control <fireflightning@gmail.com>
2017-07-09 08:24:26 -0400

    added buffer fields to Dot

--- src/main/scala/viz/lyrics/Dot.scala
+++ src/main/scala/viz/lyrics/Dot.scala
@@ -1,12 +1,17 @@
 package viz.lyrics
 
+import scala.collection.mutable.ArrayBuffer
+
 class Dot {
 
     private var _preamble = "graph g { " +
         "graph [overlap=false outputorder=edgesfirst];" +
         "node [style=filled fillcolor=white];"
     
-    private var _postscipt = "}"
+    private var _postscript = "}"
+
+    var nodeBuffer = new ArrayBuffer[Node]
+    var vertexBuffer = new ArrayBuffer[Vertex]
     
     def preamble = _preamble
     def postscript = _postscript
