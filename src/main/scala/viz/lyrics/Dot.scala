f2bc509de7e4761606158a50350a7423d28e04eb
Master Control <fireflightning@gmail.com>
2017-07-09 08:17:40 -0400

    added postscript to Dot

--- src/main/scala/viz/lyrics/Dot.scala
+++ src/main/scala/viz/lyrics/Dot.scala
@@ -6,9 +6,13 @@ class Dot {
         "graph [overlap=false outputorder=edgesfirst];" +
         "node [style=filled fillcolor=white];"
     
+    private var _postscipt = "}"
+    
     def preamble = _preamble
+    def postscript = _postscript
     
     def preamble_= (in:String):Unit = _preamble = in
+    def postscript_= (in:String):Unit = _postscript = in
 
 }
 
