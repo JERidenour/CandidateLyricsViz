0dba400a4bd09e2a7fc6dede910930f9a0055541
Master Control <fireflightning@gmail.com>
2017-07-17 08:28:59 -0400

    set drawing parameters in CandidateCluster.main()

--- src/main/scala/viz/lyrics/Dot.scala
+++ src/main/scala/viz/lyrics/Dot.scala
@@ -4,48 +4,38 @@ import scala.collection.mutable.ArrayBuffer
 
 class Dot {
 
-    private var _graphParam = "[overlap=false outputorder=edgesfirst size=24 bgcolor=gray];"
+    private var _graphParam = "[overlap=false " + 
+        "outputorder=edgesfirst " +
+        "size=24 bgcolor=gray];"
     private var _edgeParam = "[weight=2, penwidth=5];"
-    private var _nodeParam = "[fontsize=32 width=5 shape=ellipse style=filled fillcolor=white];"
+    private var _nodeParam = "[fontsize=32 width=5 " + 
+        "shape=ellipse style=filled fillcolor=white];"
 
-    private var _preamble = "graph g { " +
         //"graph [overlap=false outputorder=edgesfirst size=24];" + //CandidateCluster
         //"node [style=filled fillcolor=white];" //CandidateCluster
-        "graph" + _graphParam +
-        "edge" + _edgeParam +
-        "node" + _nodeParam 
-    
-    private var _postscript = "}"
 
     var nodeBuffer = new ArrayBuffer[Node]
     var edgeBuffer = new ArrayBuffer[Edge]
     
-    def preamble = _preamble
     def graphParam = _graphParam
     def nodeParam = _nodeParam
     def edgeParam = _edgeParam
-    def postscript = _postscript
     
-    def preamble_= (in:String):Unit = _preamble = in
     def graphParam_= (in:String):Unit = _graphParam = in
     def edgeParam_= (in:String):Unit = _edgeParam = in
     def nodeParam_= (in:String):Unit = _nodeParam = in
-    def postscript_= (in:String):Unit = _postscript = in
 
     override def toString: String = {
 
-        var s = this.preamble
-        s += this.nodeBuffer.mkString
-        s += this.edgeBuffer.distinct.mkString
-        s += this.postscript
-        s
-    }
-
-    def setPreamble(){
-        this.preamble = "graph g {" + 
+        var s = "graph g { " +
             "graph" + _graphParam +
             "edge" + _edgeParam +
             "node" + _nodeParam 
+
+        s += this.nodeBuffer.mkString
+        s += this.edgeBuffer.distinct.mkString
+        s += "}"
+        s
     }
 }
 
@@ -57,7 +47,6 @@ object Dot {
         d.graphParam = gs
         d.edgeParam = es
         d.nodeParam = ns
-        d.setPreamble
         d
     }
 }
